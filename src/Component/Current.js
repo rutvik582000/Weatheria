cce762c59c81a42a573cc3a2cc4ee5806d563612
Rutvik Navadiya <103628438+rutvik582000@users.noreply.github.com>
2022-06-18 17:35:00 +0530

    added both file

---
+++ src/Component/Current.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+
+export default class WeatherItems extends Component {
+  render() {
+    // let {this.props.response} = this.props;
+    return (
+      <>
+        <div className="container">
+          <h3 className="text-center">
+            <img src={this.props.response.current.condition.icon} alt="img" />
+            {this.props.response.location.name}, {this.props.response.location.country}
+          </h3>
+          <p className="text-center">{this.props.response.location.localtime}</p>
+        </div>
+
+        <div className="container">
+          <h5>Current Weather</h5>
+          <hr />
+          <ul>
+            <li>Weather is {this.props.response.current.condition.text}</li>
+            <li>Temperature is {this.props.response.current.temp_c} degree centigrade</li>
+            <li>Pressure is about {this.props.response.current.pressure_mb} mb</li>
+            <li>Sign of cloud is {this.props.response.current.cloud}</li>
+            <li>Wind speed is about {this.props.response.current.wind_kph} km/h</li>
+          </ul>        
+          <h5>Air Quality</h5>
+          <ul>
+            <li>Carbon monoxide level in air is {this.props.response.current.air_quality.co.toString().split('.')[0]} ppm</li>
+            <li>Nitrogene dioxide level in air is {this.props.response.current.air_quality.no2.toString().split('.')[0]} ppm</li>
+            <li>Ozone level in air is {this.props.response.current.air_quality.o3.toString().split('.')[0]} ppm</li>
+            <li>Sulfur dioxide level in air is {this.props.response.current.air_quality.so2.toString().split('.')[0]} ppm</li>
+            <li>Fine particulate matter  level in air is {this.props.response.current.air_quality.pm2_5.toString().split('.')[0]} ppm</li>
+            <li>Aerosols level in air is {this.props.response.current.air_quality.pm10.toString().split('.')[0]} ppm</li>
+          </ul>
+        </div>        
+      </>
+    );
+  }
+}
---
+++ src/Component/Forcast.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+
+export default class Forcast extends Component {
+  render() {
+    return (
+    <div className="container">
+        <h5>Tomorrow Weather Forcast</h5>
+        <hr />
+        <p>Weather will be {this.props.response.forecast.forecastday[0].day.condition.text} tomorrow
+            <img src={this.props.response.forecast.forecastday[0].day.condition.icon} alt="img" />
+        </p>
+        <ul>
+            <li>Sunrise time : {this.props.response.forecast.forecastday[0].astro.sunrise}</li>
+            <li>Sunset time : {this.props.response.forecast.forecastday[0].astro.sunset}</li>
+            <li>Moonrise time : {this.props.response.forecast.forecastday[0].astro.moonrise}</li>
+            <li>Moonset time : {this.props.response.forecast.forecastday[0].astro.moonset}</li>
+            <li>Maximum temperature will be {this.props.response.forecast.forecastday[0].day.maxtemp_c} degree centigrade</li>
+            <li>Minimum temperature will be {this.props.response.forecast.forecastday[0].day.mintemp_c} degree centigrade</li>
+            <li>Maximum wind speed will be {this.props.response.forecast.forecastday[0].day.maxwind_kph} km/h</li>
+            <li>Chance of rain and snow will be {this.props.response.forecast.forecastday[0].day.daily_chance_of_rain} and {this.props.response.forecast.forecastday[0].day.daily_chance_of_snow} </li>
+        </ul>
+    </div>
+    )
+  }
+}
